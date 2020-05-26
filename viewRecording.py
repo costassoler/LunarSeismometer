2b8323ac996a6b36b545312f1f46a2ec3c40a9e7
eluum <bhrumbaugh@yahoo.com>
2020-05-26 12:09:51 -0700

    fixed some minor thing idk

--- viewRecording.py
+++ viewRecording.py
@@ -1,4 +1,6 @@
-from imports import *
+import sys
+import intByteWrite as ibw
+from matplotlib import pyplot as plt
 
 # get filename from argument
 fileName = sys.argv[1]
@@ -6,5 +8,8 @@ fileName = sys.argv[1]
 # load file data
 fileData = ibw.load(fileName,channels=2)
 
-plt.plot(fileData[0],fileData[1]) 
+print(fileData[:100,:])
+
+plt.figure()
+plt.plot(fileData[:7 *60 * 60 * 40,0],fileData[:7 * 60 * 60 * 40,1]) 
 plt.show()
